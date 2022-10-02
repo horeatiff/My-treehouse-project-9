35f05b8342cf096e7e89fcce080d96ccf387f9ee
Horea Tiff <horea.tiff@gmail.com>
2022-10-02 21:06:10 +0300

    Added "about me" section

--- script/app.js
+++ script/app.js
@@ -1,9 +1,9 @@
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
+function buttonLinks() {
+  var x = document.getElementById("button-links");
+  if (x.style.display === "flex") {
     x.style.display = "none";
   } else {
-    x.style.display = "block";
+    x.style.display = "flex";
   }
 }
 
